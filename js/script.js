57187f96b90dac057a136fdf10f1c9ec574e94a4
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2024-11-18 12:28:42 +0530

    Add the logic to play a single round
    Make the function's humanChoice parameters case-insensitive.

--- js/script.js
+++ js/script.js
@@ -9,22 +9,55 @@ function getComputerChoice() {
   let randomNumber = getRandomIntInclusive(0, 2);
   switch (randomNumber) {
     case 0:
-      return "rock";
+      return "Rock";
     case 1:
-      return "paper";
+      return "Paper";
     case 2:
-      return "scissors";
+      return "Scissors";
   }
 }
 
-console.log(getComputerChoice());
-
-function getHumanChoice() {
-  let userChoice = prompt('Enter either "rock", "paper" or "scissors":', "rock");
-  return userChoice;
+function cleanHumanChoice(userChoice) {
+  const userChoiceLowerCased = userChoice.toLowerCase();
+  const firstLetterCapitalized = userChoiceLowerCased.charAt(0).toUpperCase();
+  return firstLetterCapitalized.concat(userChoiceLowerCased.slice(1));
 }
 
-console.log(getHumanChoice());
+function getHumanChoice() {
+  let userChoice = prompt("Enter 'Rock', 'Paper' or 'Scissors'");
+  return cleanHumanChoice(userChoice);
+}
 
-let computerScore = 0;
 let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+  } else {
+    if (humanChoice === "Rock" && computerChoice === "Paper") {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+      computerScore++;
+    } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+      humanScore++;
+    } else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+      computerScore++;
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+      humanScore++;
+    } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+      computerScore++;
+    } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+      humanScore++;
+    }
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
