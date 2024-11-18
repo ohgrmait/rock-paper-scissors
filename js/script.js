fb876462796043b8ccc2bccdaa3b99dfba34c618
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2024-11-18 09:35:38 +0530

    Add the logic to randomly get the computer choice
    Add a function to randomly return one of the string values: "rock",
    "paper" or "scissors".

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("Rock Paper Scissors!");
+function getRandomIntInclusive(min, max) {
+  // Taken from https://shorturl.at/TSjlB
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+
+function getComputerChoice() {
+  let randomNumber = getRandomIntInclusive(0, 2);
+  switch (randomNumber) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+}
+
+console.log(getComputerChoice());
