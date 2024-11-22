cf672e2c03253491f2a5c11b77667ff43f50c1ab
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2024-11-22 11:34:57 +0530

    Create smaller functions to separate concerns

--- js/script.js
+++ js/script.js
@@ -1,24 +1,14 @@
-function getRandomIntInclusive(min, max) {
-  // Taken from https://shorturl.at/TSjlB
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
-}
-
-function getComputerChoice() {
-  let randomNumber = getRandomIntInclusive(0, 2);
-  switch (randomNumber) {
-    case 0:
-      return "Rock";
-    case 1:
-      return "Paper";
-    case 2:
-      return "Scissors";
+function declareWinner() {
+  if (humanScore === 5) {
+    winner.textContent = `Yahoo! You won.`;
+    winnerAudio.play();
+    isGameOver = true;
+  } else if (computerScore === 5) {
+    winner.textContent = `Boohoo! Computer won.`;
+    loserAudio.play();
+    isGameOver = true;
   }
 }
-
-let humanScore = 0;
-let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
   let result = "";
@@ -48,6 +38,42 @@ function playRound(humanChoice, computerChoice) {
   return result;
 }
 
+function getRandomIntInclusive(min, max) {
+  // Taken from https://shorturl.at/TSjlB
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+
+function getComputerChoice() {
+  let randomNumber = getRandomIntInclusive(0, 2);
+  switch (randomNumber) {
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    case 2:
+      return "Scissors";
+  }
+}
+
+function resetGame(e) {
+  if (e.target.textContent === 'Reset') {
+    humanScore = 0;
+    computerScore = 0;
+    resetAudio.play();
+    result.textContent = '';
+    score.textContent = '';
+    winner.textContent = '';
+    isGameOver = false;
+  }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+let isGameOver = false;
+
 const buttons = document.querySelectorAll(".btn");
 const result = document.querySelector(".result");
 const score = document.querySelector(".score");
@@ -58,18 +84,10 @@ const winnerAudio = document.querySelector("#winner-audio");
 const loserAudio = document.querySelector("#loser-audio");
 const resetAudio = document.querySelector("#reset-audio");
 
-let isGameOver = false;
-
 buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    if (button.textContent === 'Reset') {
-      isGameOver = false;
-      resetAudio.play();
-      humanScore = 0;
-      computerScore = 0;
-      result.textContent = '';
-      score.textContent = '';
-      winner.textContent = '';
+  button.addEventListener("click", function (e) {
+    if (e.target.textContent === 'Reset') {
+      resetGame(e);
       return;
     }
 
@@ -79,21 +97,12 @@ buttons.forEach((button) => {
 
     audio.play();
 
-    let humanSelection = button.textContent;
+    let humanSelection = e.target.textContent;
     let computerSelection = getComputerChoice();
-
     result.textContent = playRound(humanSelection, computerSelection);
 
     score.textContent = `Score: You: ${humanScore}, Computer: ${computerScore}`;
 
-    if (humanScore === 5) {
-      winner.textContent = `Yahoo! You won.`;
-      winnerAudio.play();
-      isGameOver = true;
-    } else if (computerScore === 5) {
-      winner.textContent = `Boohoo! Computer won.`;
-      loserAudio.play();
-      isGameOver = true;
-    }
+    declareWinner();
   });
 });
